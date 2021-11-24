a2a7364dea845663e9bc886793fc649fd0816ae5
abailey76 <abailey76@duck.com>
2021-11-23 21:59:54 -0600

    caught up with studio review vid and ready for next step

---
+++ RestaurantMenu/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RestaurantMenu
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MenuItem borger = new MenuItem("Borger", 4.20, "Tasty Borger", "Borger", true);
+
+            Console.WriteLine(borger.Description);
+        }
+    }
+}
