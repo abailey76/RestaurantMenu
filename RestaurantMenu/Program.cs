9502f01a307073812f14fcc89cdb64c291b53664
abailey76 <abailey76@duck.com>
2021-11-29 21:19:14 -0600

    fleshing out the menu a little more

--- RestaurantMenu/Program.cs
+++ RestaurantMenu/Program.cs
@@ -6,13 +6,19 @@ namespace RestaurantMenu
     {
         static void Main(string[] args)
         {
-            Menu menu = new Menu("Adam's Restaurant");
-            Item item1 = new Item("Turkey Taco", "Dessert", "Made with genuine Thanksgiving leftovers", 12.99);
-            Item item2 = new Item("Turkey Sandwich", "Main Course", "Tryptophan on a Bun", 12.99);
-            Item item3 = new Item("Turkey Salad", "Appetizer", "Thanksgiving Starter", 12.99, true);            
+            Menu menu = new Menu("Alice's Restaurant: You can get anything you want...");
+            Item item1 = new Item("Pecan Pie", "Dessert", "Our word-famous, top secret recipe.", 2.99);
+            Item item2 = new Item("Turkey Meatloaf", "Main Course", "Basically, it's a tryptophan grenade.", 5.99, true);
+            Item item3 = new Item("Chop Salad", "Appetizer", "Probably the healthiest thing on the menu.", 3.99, true);
+            Item item4 = new Item("Chocolate Ice Cream", "Dessert", "Everyone's favorite flavor.", 1.99, true);
+            Item item5 = new Item("Spicy Turkey Bowl", "Main Course", "The OTHER kickin' chicken", 5.99);
+            Item item6 = new Item("Fried Cheese Sticks", "Appetizer", "Who doesn't love deep fried cheese?", 4.99);
             menu.Items.Add(item1);
             menu.Items.Add(item2);
             menu.AddItem(item3);
+            menu.AddItem(item4);
+            menu.AddItem(item5);
+            menu.AddItem(item6);
             menu.PrintMenu();
         }
     }
